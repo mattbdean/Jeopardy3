1099740121b6d5ca3196c187b75eee725cb1166b
Matthew Dean <deanmatthew16@gmail.com>
2013-09-28 14:42:44 -0400

    Basic view of the game where the answer is shown instead of the point value (to be changed)

--- index.php
+++ index.php
@@ -1,54 +1,54 @@
 <?php
+require 'answer.class.php';
 // Temporary variables for testing
 
-$gameId = "0";
+$gameId = "sample";
 
 function makeGameFor($gameId) {
+	$ROW_COUNT = 5;
+
 	// Parse an XML file from games/${id}.xml
-	$file = "games/$gameId.xml";
-	$xml = simplexml_load_file($file) or die("Could not load file");
+	$xml = simplexml_load_file("games/$gameId.xml") or die("I couldn't seem to find that game. Sorry :\\");
 
-	// ob_start();
+	ob_start();
+	echo '<table id="gameboard">';
 
-	// For each column element,
-	$columnAmount = count($xml->children());
-	foreach ($xml->children() as $column) {
-		echo '<div class="column" style="max-width: ' . 100 / $columnAmount . '%">';
-		echo '<div class="column-header">';
-		echo $column->attributes()['name'];
-		// End column-header
-		echo '</div>';
-		// For each answerContainer in the column,
-		// for ($column->children() as $answerContainer) {
-		$columnChildren = $column->children();
-		for ($i = 0; $i < count($columnChildren); $i++) {
-			$answerContainer = $columnChildren[$i];
-			echo '<div class="column-answer">';
-			echo '$' . (($i + 1) * 100) . '<br>';
-			// End column-answer
-			echo '</div>';
-		}
-
-		// End column
-		echo '</div>';
+	// Print the category headers
+	echo '<tr>';
+	foreach ($xml->column as $column) {
+		echo '<th>' . $column->attributes()['name'] . '</th>';
 	}
-	// ob_end_flush();
+	echo '</tr>';
+
+	// Print the questions
+	for ($i = 0; $i < $ROW_COUNT; $i++) {
+		echo '<tr>';
+		foreach ($xml->column as $column) {
+			echo '<td>' . $column->answerContainer[$i]->answer . '</td>';
+		}
+		echo '</tr>';
+	}
+
+	echo '</table>';
+
+	ob_end_flush();
 }
 ?>
 
 <html>
 <head>
 	<title>Jeopardy 3</title>
+	<link href='http://fonts.googleapis.com/css?family=Droid+Serif' rel='stylesheet' type='text/css'>
+	<link rel="stylesheet" href="res/font/gyparody/stylesheet.css">
 	<link rel="stylesheet" href="res/styles/index.css">
+
 </head>
 <body>
 	<div id="content-wrapper">
 		<div id="header">
 			<h1 id="header-text">Jeopardy!</h1>
 		</div>
-		<div id="gameboard">
-			<?php makeGameFor($gameId) ?>
-		</div>
+		<?php makeGameFor($gameId) ?>
 	</div>
 </body>
 </html>
